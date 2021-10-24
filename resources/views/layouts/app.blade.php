867c1a3bc493885edcecd4c0aa6cbbce81ab93b3
Iraldin <l.chausiaux@outlook.com>
2021-10-24 20:53:27 +0200

    Layout homepage menu wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,65 +7,67 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body class="antialiased">
-        <div class="navbar">
-            <ul>
-                <li class="list">
-                    <b></b>
-                    <b></b>
-                    <a href="#">
+        <div class="navbar-wrapper">
+            <div class="navbar">
+                <ul>
+                    <li class="list">
+                        <b></b>
+                        <b></b>
+                        <a href="{{ route('menu.index') }}">
                         <span class="icon">
                             <ion-icon name="home-outline"></ion-icon>
                         </span>
-                        <span class="title">Home</span>
-                    </a>
-                </li>
-                <li class="list">
-                    <b></b>
-                    <b></b>
-                    <a href="#">
+                            <span class="title">Menu</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <b></b>
+                        <b></b>
+                        <a href="#">
                         <span class="icon">
                             <ion-icon name="person-outline"></ion-icon>
                         </span>
-                        <span class="title">Profile</span>
-                    </a></li>
-                <li class="list active">
-                    <b></b>
-                    <b></b>
-                    <a href="#">
+                            <span class="title">Profile</span>
+                        </a></li>
+                    <li class="list active">
+                        <b></b>
+                        <b></b>
+                        <a href="#">
                         <span class="icon">
                             <ion-icon name="chatbox-outline"></ion-icon>
                         </span>
-                        <span class="title">Messages</span>
-                    </a>
-                </li>
-                <li class="list">
-                    <b></b>
-                    <b></b>
-                    <a href="#">
+                            <span class="title">Messages</span>
+                        </a>
+                    </li>
+                    <li class="list">
+                        <b></b>
+                        <b></b>
+                        <a href="#">
                         <span class="icon">
                             <ion-icon name="settings-outline"></ion-icon>
                         </span>
-                        <span class="title">Settings</span>
-                    </a></li>
-                <li class="list">
-                    <b></b>
-                    <b></b>
-                    <a href="#">
+                            <span class="title">Settings</span>
+                        </a></li>
+                    <li class="list">
+                        <b></b>
+                        <b></b>
+                        <a href="#">
                         <span class="icon">
                             <ion-icon name="help-circle-outline"></ion-icon>
                         </span>
-                        <span class="title">Help</span>
-                    </a></li>
-                <li class="list">
-                    <b></b>
-                    <b></b>
-                    <a href="#">
+                            <span class="title">Help</span>
+                        </a></li>
+                    <li class="list">
+                        <b></b>
+                        <b></b>
+                        <a href="#">
                         <span class="icon">
                             <ion-icon name="log-out-outline"></ion-icon>
                         </span>
-                        <span class="title">Logout</span>
-                    </a></li>
-            </ul>
+                            <span class="title">Logout</span>
+                        </a></li>
+                </ul>
+            </div>
         </div>
         <div class="wrapper">
             <div class="toggle"></div>
@@ -74,18 +76,6 @@
         <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
         <script>
-            let list = document.querySelectorAll('.list');
-            for (let i = 0; i < list.length; i++) {
-                list[i].onclick = function ()
-                {
-                    let j = 0;
-                    while (j < list.length) {
-                        list[j++].className = 'list'
-                    }
-                    list[i].className = 'list active'
-                }
-            }
-
             let menuToggle = document.querySelector('.toggle');
             let navbar = document.querySelector('.navbar');
             let wrapper = document.querySelector('.wrapper');
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Menu\MenuController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('menu')->name('menu.')->group(function() {
+    Route::get('/', [MenuController::class, 'index'])->name('index');
 });
